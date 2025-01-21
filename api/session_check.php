52f020251abaaaed60c6f64c1575d8bbdcc7f30d
Fatiha <atiera2002@gmail.com>
2025-01-21 09:49:16 +0800

    update inactive to 15 second

--- api/session_check.php
+++ api/session_check.php
@@ -2,7 +2,7 @@
 session_start(); 
 
 // Session timeout logic 
-$inactive = 5; // testing 5 seconds 
+$inactive = 15; // testing 15 seconds 
 if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > $inactive)) { 
     session_unset();  
     session_destroy();  
